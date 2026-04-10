0bdc56b35424d101cf8552785187ec66cc1a97cc
Павел Слабкий <pavelslabkiy@MacBook-Air-4.local>
2026-04-10 16:29:11 +0300

    version fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    })
+  ],
   server: {
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
 })
